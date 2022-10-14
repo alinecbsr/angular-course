94700e3987daaa88298f35e7e242bb8ebc4cbd47
alinecbsr <alinecbsr@gmail.com>
2022-10-14 07:32:43 +0100

    Direvtive ngSwitch

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
+import { DirectiveNgswitchComponent } from './directive-ngswitch/directive-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveNgifComponent
+    DirectiveNgifComponent,
+    DirectiveNgswitchComponent
   ],
   imports: [
     BrowserModule
---
+++ directives/src/app/directive-ngswitch/directive-ngswitch.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DirectiveNgswitchComponent } from './directive-ngswitch.component';
+
+describe('DirectiveNgswitchComponent', () => {
+  let component: DirectiveNgswitchComponent;
+  let fixture: ComponentFixture<DirectiveNgswitchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DirectiveNgswitchComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DirectiveNgswitchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
